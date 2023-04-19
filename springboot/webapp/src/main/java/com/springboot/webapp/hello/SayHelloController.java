c0613fa12f2ea466bada4df7aca496b5ab1c20e4
Franko Vekić <franko.vekic@gmail.com>
2023-04-20 01:31:26 +0200

    First Spring MVC Controller, @ResponseBody, @Controller

---
+++ springboot/webapp/src/main/java/com/springboot/webapp/hello/SayHelloController.java
@@ -0,0 +1,18 @@
+package com.springboot.webapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+	//http://localhost:8080/say-hello
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello! What are you learning today?";
+	}
+	
+}
+
