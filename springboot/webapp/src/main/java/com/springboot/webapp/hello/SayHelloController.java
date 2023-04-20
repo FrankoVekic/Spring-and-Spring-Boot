2d065d2f2320124f0b071db9b97aa0140163a95f
Franko Vekić <franko.vekic@gmail.com>
2023-04-20 02:02:19 +0200

    Redirect to a JSP using Spring Boot - Controller, @ResponseBody & View

--- springboot/webapp/src/main/java/com/springboot/webapp/hello/SayHelloController.java
+++ springboot/webapp/src/main/java/com/springboot/webapp/hello/SayHelloController.java
@@ -7,30 +7,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SayHelloController {
 
-	//http://localhost:8080/say-hello
+	// http://localhost:8080/say-hello
 	@RequestMapping("say-hello")
 	@ResponseBody
 	public String sayHello() {
 		return "Hello! What are you learning today?";
 	}
-	
-	//http://localhost:8080/say-hello
-		@RequestMapping("say-hello-html")
-		@ResponseBody
-		public String sayHelloHtml() {
-			
-			StringBuffer sb = new StringBuffer();
-			sb.append("<html>");
-			sb.append("<head>");
-			sb.append("<title> My first HTML Page </title>");
-			sb.append("</head>");
-			sb.append("<body>");
-			sb.append("My first html page with body");
-			sb.append("</body>");
-			sb.append("</html>");
-			
-			return sb.toString();
-		}
-	
-}
 
+	// http://localhost:8080/say-hello-html
+	@RequestMapping("say-hello-html")
+	@ResponseBody
+	public String sayHelloHtml() {
+
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title> My first HTML Page </title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("My first html page with body");
+		sb.append("</body>");
+		sb.append("</html>");
+
+		return sb.toString();
+	}
+
+	// JSP (Java Server Pages)
+	// say-hello-jsp => sayHello.jsp
+	// http://localhost:8080/say-hello-jsp
+	@RequestMapping("say-hello-jsp")
+	public String sayHelloJsp() {
+		return "sayHello";
+	}
+}
