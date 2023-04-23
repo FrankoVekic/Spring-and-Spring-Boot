6ec130a66cd019216032958ab762c7358ada7bfe
Franko Vekić <franko.vekic@gmail.com>
2023-04-23 20:47:52 +0200

    Getting started with Todo Features - Creating Todo and TodoService

---
+++ springboot/webapp/src/main/java/com/springboot/webapp/todo/TodoService.java
@@ -0,0 +1,23 @@
+package com.springboot.webapp.todo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class TodoService {
+
+	private static List<Todo> todos;
+	static {
+		todos.add(new Todo(1, "Franko", "Learn AWS", 
+				LocalDate.now().plusYears(1), false));
+		
+		todos.add(new Todo(1, "Franko", "Learn DevOps", 
+				LocalDate.now().plusYears(2), false));
+		
+		todos.add(new Todo(1, "Franko", "Learn Full Stack Development", 
+				LocalDate.now().plusYears(3), false));
+	}
+	
+	public List<Todo> findByUsername(String username){
+		return todos;
+	}
+}
