f404dd2bcd3abf16f270c55ef48ec6c66b43604d
Franko Vekić <franko.vekic@gmail.com>
2023-05-20 15:17:42 +0200

    Implementing Validations for REST API

--- springboot/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ springboot/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.rest.webservices.restfulwebservices.exception;
 
+import java.net.http.HttpHeaders;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -24,11 +27,23 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	}
 
 	@ExceptionHandler(UserNotFoundException.class)
-	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request)
+			throws Exception {
 		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),
 				request.getDescription(false));
 
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			org.springframework.http.HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getFieldError().getDefaultMessage(),
+				request.getDescription(false));
+
+		return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+
 	}
 
 }
