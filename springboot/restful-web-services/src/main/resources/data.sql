0fcaa4bab98d80ab0434cba848e382de0cd74f9d
Franko Vekić <franko.vekic@gmail.com>
2023-05-22 21:52:06 +0200

    Creating User Entity and some test data

---
+++ springboot/restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_details (id, name, birth_date) 
+values(10001,'Franko', current_date());
+
+insert into user_details (id, name, birth_date) 
+values(10002,'Stanko', current_date());
+
+insert into user_details (id, name, birth_date) 
+values(10003,'Branko', current_date());
