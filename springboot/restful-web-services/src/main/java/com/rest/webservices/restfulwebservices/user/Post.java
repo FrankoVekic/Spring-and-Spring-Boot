b30e7f7d911986fac4c7bba4ad4c15c8b3ab9d33
Franko Vekić <franko.vekic@gmail.com>
2023-05-23 21:50:04 +0200

    Correcting typo

--- springboot/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/Post.java
+++ springboot/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/Post.java
@@ -29,17 +29,17 @@ public class Post {
 		this.id = id;
 	}
 
-	public String getDescripton() {
-		return descripton;
+	public String getDescription() {
+		return description;
 	}
 
-	public void setDescripton(String descripton) {
-		this.descripton = descripton;
+	public void setDescripton(String description) {
+		this.description = description;
 	}
 
 	@Override
 	public String toString() {
-		return "Post [id=" + id + ", descripton=" + descripton + "]";
+		return "Post [id=" + id + ", description=" + description + "]";
 	}
 
 }
