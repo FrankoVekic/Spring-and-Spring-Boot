1724c942bf34cfa9fcc4e30085f24225944f2a7f
Franko Vekić <franko.vekic@gmail.com>
2023-06-10 17:55:39 +0200

    Setting up Counter and Counter Button

--- springboot/react-counter/view/counter-app/src/components/counter/counter.jsx
+++ springboot/react-counter/view/counter-app/src/components/counter/counter.jsx
@@ -2,7 +2,26 @@ import { useState } from 'react'
 import {PropTypes} from 'prop-types'
 import './Counter.css'
 
-export default function Counter({by}){
+export default function Counter(){
+
+    const [count, setCount] = useState(0)
+
+    function incrementCounterParentFunction(by){
+        setCount(count + by)
+
+    }
+
+    return (
+        <>
+        <span className="totalCount">{count}</span>
+        <CounterButton by={1}/>
+        <CounterButton by={2}/>
+        <CounterButton by={5}/>
+        </>
+    )
+}
+
+function CounterButton({by}){
 
     const [count, setCount] = useState(0);
 
@@ -28,10 +47,10 @@ export default function Counter({by}){
 
 }
 
-Counter.propTypes = {
+CounterButton.propTypes = {
     by: PropTypes.number
 }
 
-Counter.defaultProps = {
+CounterButton.defaultProps = {
     by: 1
 }
