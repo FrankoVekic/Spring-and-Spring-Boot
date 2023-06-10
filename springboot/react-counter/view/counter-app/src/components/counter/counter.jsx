eb2dedf14992a9afc4a271eb6ef436ce4d9ae5f9
Franko Vekić <franko.vekic@gmail.com>
2023-06-10 18:24:44 +0200

    CounterButton as separate module

--- springboot/react-counter/view/counter-app/src/components/counter/counter.jsx
+++ springboot/react-counter/view/counter-app/src/components/counter/counter.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import {PropTypes} from 'prop-types'
 import './Counter.css'
+import CounterButton from './CounterButton'
 
 export default function Counter(){
 
     const [count, setCount] = useState(0)
+
 
     function incrementCounterParentFunction(by){
         setCount(count + by)
@@ -36,37 +37,3 @@ export default function Counter(){
     )
 }
 
-function CounterButton({by, incrementMethod, decrementMethod}){
-
-    const [count, setCount] = useState(0);
-
-    function incrementCounterFunction(){     
-        setCount(count + by)
-        incrementMethod(by)
-    }
-
-    function decrementCounterFunction(){
-        setCount(count - by) 
-        decrementMethod(by)  
-    }
-
-    return (
-            <div className="Counter">
-                <div>
-                    <button className="counterButton" 
-                            onClick={incrementCounterFunction}>+{by}</button>
-                     <button className="counterButton" 
-                            onClick={decrementCounterFunction}>-{by}</button>
-                </div>
-            </div>
-    )
-
-}
-
-CounterButton.propTypes = {
-    by: PropTypes.number
-}
-
-CounterButton.defaultProps = {
-    by: 1
-}
