baa8cf6a1e2b4bf9acf1ed556d9e9d220b60ae98
Franko Vekić <franko.vekic@gmail.com>
2023-06-15 01:17:19 +0200

    Creating React Header, Footer and Logout Components

--- springboot/todo-management-and-counter/view/counter-app/src/components/todo/TodoApp.jsx
+++ springboot/todo-management-and-counter/view/counter-app/src/components/todo/TodoApp.jsx
@@ -5,16 +5,19 @@ import './TodoApp.css'
 export default function TodoApp(){
     return (
         <div className="TodoApp">
+            <HeaderComponent />
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<LoginComponent />} />
                     <Route path='/login' element={<LoginComponent />} />
                     <Route path='/welcome/:username' element={<WelcomeComponent />} />     
                     <Route path='/todos' element={<ListTodosComponent />} />  
+                    <Route path='/logout' element={<LogoutComponent />} />  
 
                     <Route path='*' element={<ErrorComponent />} />               
                 </Routes>
-            </BrowserRouter>           
+            </BrowserRouter>  
+            <FooterComponent />         
         </div>
     )
 }
@@ -138,6 +141,37 @@ function ListTodosComponent(){
                         }
                     </tbody>
                 </table>
+            </div>
+        </div>
+    )
+}
+
+function HeaderComponent(){
+    return (
+        <div className="header">
+            <div>
+                Header <hr/>
+            </div>
+        </div>
+    )
+}
+
+function FooterComponent(){
+    return (
+        <div className="footer">
+            <div>
+               <hr/> Footer
+            </div>
+        </div>
+    )
+}
+
+function LogoutComponent(){
+    return (
+        <div className="LogoutComponent">
+            <h1>You are logged out!</h1>
+            <div>
+               Thank you for using our App. Come back soon!
             </div>
         </div>
     )
