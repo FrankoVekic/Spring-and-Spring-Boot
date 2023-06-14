1b085531468a11cb39759cdb12186100dd44409b
Franko Vekić <franko.vekic@gmail.com>
2023-06-15 01:11:40 +0200

    Displaying More Todo Details in React List Todo Component

--- springboot/todo-management-and-counter/view/counter-app/src/components/todo/TodoApp.jsx
+++ springboot/todo-management-and-counter/view/counter-app/src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {BrowserRouter, Routes, Route, useNavigate, useParams} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, useNavigate, useParams, Link} from 'react-router-dom'
 import './TodoApp.css'
 
 export default function TodoApp(){
@@ -82,7 +82,7 @@ function WelcomeComponent(){
         <div className="WelcomeComponent">
             <h1>Welcome {username}</h1>
         <div>
-            Welcome Component
+           Manage your todos -  <Link to="/todos">Go here</Link>
         </div>
         </div>
     )
@@ -101,10 +101,13 @@ function ErrorComponent(){
 
 function ListTodosComponent(){
 
+    const today = new Date();
+    const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
+
     const todos = [
-        {id: 1, description: 'Learn Spring'},
-        {id: 2, description: 'Learn Full Stack Dev'},
-        {id: 3, description: 'Learn DevOps'}
+        {id: 1, description: 'Learn Spring', done: false, targetDate: targetDate},
+        {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate: targetDate},
+        {id: 3, description: 'Learn DevOps', done: false, targetDate: targetDate}
     ]
     
     return (
@@ -114,8 +117,10 @@ function ListTodosComponent(){
                 <table>
                     <thead>
                         <tr>
-                            <td>id</td>
-                            <td>description</td>
+                            <td>ID</td>
+                            <td>Description</td>
+                            <td>Is Done?</td>
+                            <td>Target Date</td>
                         </tr>
                     </thead>
                     <tbody>
@@ -125,6 +130,8 @@ function ListTodosComponent(){
                                 <tr key={todo.id}>
                                     <td>{todo.id}</td>
                                     <td>{todo.description}</td>
+                                    <td>{todo.done.toString()}</td>
+                                    <td>{todo.targetDate.toDateString()}</td>
                                 </tr>
                                 )
                             )
