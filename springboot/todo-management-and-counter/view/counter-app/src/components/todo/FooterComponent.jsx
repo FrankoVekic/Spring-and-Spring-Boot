dfccc7580a46875ea48bd2b143cf6f4819637bd5
Franko Vekić <franko.vekic@gmail.com>
2023-06-17 00:34:24 +0200

    Sharing React State with Multiple Components with Auth Context

--- springboot/todo-management-and-counter/view/counter-app/src/components/todo/FooterComponent.jsx
+++ springboot/todo-management-and-counter/view/counter-app/src/components/todo/FooterComponent.jsx
@@ -1,5 +1,12 @@
+import { useContext } from "react";
+import { AuthContext } from "./security/AuthContext";
 
 function FooterComponent(){
+
+    const authContext = useContext(AuthContext)
+
+    console.log(`Footer component - ${authContext.number}`);
+
     return (
         <footer className="footer">
             <div className="container">
@@ -8,3 +15,5 @@ function FooterComponent(){
         </footer>
     )
 }
+
+export default FooterComponent
