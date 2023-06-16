de489a44527bfa3a918fa799237709ac12bfc436
Franko Vekić <franko.vekic@gmail.com>
2023-06-16 23:50:13 +0200

    Refactoring Components to Individual JavaScript Modules

---
+++ springboot/todo-management-and-counter/view/counter-app/src/components/todo/ErrorComponent.jsx
@@ -0,0 +1,12 @@
+function ErrorComponent(){
+    return (
+        <div className="ErrorComponent">
+            <h1>404</h1>
+            <div>
+                Something went wrong.
+            </div>
+        </div>
+    )
+}
+
+export default ErrorComponent
