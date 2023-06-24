239e1d5e2673123379197a6fdcac56fa05c5889a
Franko Vekić <franko.vekic@gmail.com>
2023-06-24 19:09:52 +0200

    Enabling CORS Requests for Spring Boot REST API

--- springboot/restful-web-application/src/main/java/com/restful/application/restfulwebapplication/RestfulWebApplication.java
+++ springboot/restful-web-application/src/main/java/com/restful/application/restfulwebapplication/RestfulWebApplication.java
@@ -2,12 +2,26 @@ package com.restful.application.restfulwebapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestfulWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedMethods("*")
+				.allowedOrigins("http://localhost:3000");
+			}
+		};	
 	}
 
 }
