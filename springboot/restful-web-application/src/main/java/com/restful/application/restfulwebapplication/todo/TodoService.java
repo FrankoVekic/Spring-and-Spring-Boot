a86614318cd221e969ee16da09ef644c4e62f711
Franko Vekić <franko.vekic@gmail.com>
2023-07-06 20:19:19 +0200

    Creating Retrieve Todo and Delete Todo

--- springboot/restful-web-application/src/main/java/com/restful/application/restfulwebapplication/todo/TodoService.java
+++ springboot/restful-web-application/src/main/java/com/restful/application/restfulwebapplication/todo/TodoService.java
@@ -15,6 +15,7 @@ public class TodoService {
 	private static int todosCount = 0;
 	
 	static {
+		
 		todos.add(new Todo(++todosCount, "Franko","Get AWS Certified", 
 							LocalDate.now().plusYears(10), false ));
 		todos.add(new Todo(++todosCount, "Franko","Learn DevOps", 
