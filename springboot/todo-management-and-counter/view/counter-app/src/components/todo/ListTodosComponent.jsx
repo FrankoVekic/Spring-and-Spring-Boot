847d69fbbb9842a7e67f5adf7c165fcc823b6d30
Franko Vekić <franko.vekic@gmail.com>
2023-07-06 20:07:28 +0200

    Displaying Todos

--- springboot/todo-management-and-counter/view/counter-app/src/components/todo/ListTodosComponent.jsx
+++ springboot/todo-management-and-counter/view/counter-app/src/components/todo/ListTodosComponent.jsx
@@ -1,14 +1,24 @@
+import { useEffect, useState } from "react";
+import { retrieveAllTodosForUsername } from "./api/TodoApiService";
+
 function ListTodosComponent(){
 
     const today = new Date();
+
     const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
 
-    const todos = [
-        {id: 1, description: 'Learn Spring', done: false, targetDate: targetDate},
-        {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate: targetDate},
-        {id: 3, description: 'Learn DevOps', done: false, targetDate: targetDate}
-    ]
-    
+    const [todos, setTodos] = useState([])
+
+    useEffect( () => refreshTodos(), [] )
+
+function refreshTodos(){
+    retrieveAllTodosForUsername('Franko')
+        .then(response => {
+            setTodos(response.data)
+        } )
+        .catch(error => console.log(error))
+}
+
     return (
         <div className="container">
             <h1>Things You Want To DO!</h1>
@@ -30,7 +40,7 @@ function ListTodosComponent(){
                                     <td>{todo.id}</td>
                                     <td>{todo.description}</td>
                                     <td>{todo.done.toString()}</td>
-                                    <td>{todo.targetDate.toDateString()}</td>
+                                    <td>{todo.targetDate.toString()}</td>
                                 </tr>
                                 )
                             )
