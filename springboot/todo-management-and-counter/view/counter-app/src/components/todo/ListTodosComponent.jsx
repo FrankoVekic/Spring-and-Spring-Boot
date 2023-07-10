282a972c633c46217389cc89875d3d027da2e54f
Franko Vekić <franko.vekic@gmail.com>
2023-07-11 00:33:26 +0200

    Setting Username into React Auth Context

--- springboot/todo-management-and-counter/view/counter-app/src/components/todo/ListTodosComponent.jsx
+++ springboot/todo-management-and-counter/view/counter-app/src/components/todo/ListTodosComponent.jsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import { retrieveAllTodosForUsernameApi, deletTodoApi } from "./api/TodoApiService";
+import { useAuth } from "./security/AuthContext";
 
 function ListTodosComponent(){
 
     const today = new Date();
+
+    const authContext = useAuth();
+
+    const username = authContext.username
 
     const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
 
@@ -14,7 +19,7 @@ function ListTodosComponent(){
     useEffect( () => refreshTodos(), [] )
 
 function refreshTodos(){
-    retrieveAllTodosForUsernameApi('Franko')
+    retrieveAllTodosForUsernameApi(username)
         .then(response => {
             setTodos(response.data)
         } )
@@ -23,7 +28,7 @@ function refreshTodos(){
 
 function deleteTodo(id){
     console.log('clicked ' + id)
-    deletTodoApi('Franko', id)
+    deletTodoApi(username, id)
     .then(
         () => {
             setMessage(`Todo with id = ${id} is successfuly deleted`)
