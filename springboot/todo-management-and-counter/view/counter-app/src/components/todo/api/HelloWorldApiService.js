21089ea0bfffa8548ea6b56fc6cfe2c113e3a13c
Franko Vekić <franko.vekic@gmail.com>
2023-07-20 21:49:14 +0200

    Adding Authorization Header in React to Spring Boot REST API calls

--- springboot/todo-management-and-counter/view/counter-app/src/components/todo/api/HelloWorldApiService.js
+++ springboot/todo-management-and-counter/view/counter-app/src/components/todo/api/HelloWorldApiService.js
@@ -15,4 +15,8 @@ export const retrieveHelloWorldBean
     = () => apiClient.get('/hello-world-bean')
 
     export const retrieveHelloWorldPathVariable 
-    = (username) => apiClient.get(`/hello-world/path-variable/${username}`)
+    = (username) => apiClient.get(`/hello-world/path-variable/${username}`, {
+        headers: {
+            Authorization: 'Basic RnJhbmtvOnRlc3Q='
+        }
+    })
